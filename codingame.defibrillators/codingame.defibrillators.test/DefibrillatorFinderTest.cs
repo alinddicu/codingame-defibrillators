a0b8d08d2f0444f52f2e247402bc584eb410a87b
alinddicu <alinddicu@yahoo.com>
2016-03-12 15:24:12 +0100

    ok

---
+++ codingame.defibrillators/codingame.defibrillators.test/DefibrillatorFinderTest.cs
@@ -0,0 +1,33 @@
+﻿namespace codingame.defibrillators.test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class DefibrillatorFinderTest
+    {
+        private DefibrillatorFinder _finder;
+
+        [TestMethod]
+        public void Test()
+        {
+            var longiture = "3,879483";
+            var latitude = "43,608177";
+            var defibLines = new[]
+            {
+                "1;Maison de la Prevention Sante;6 rue Maguelone 340000 Montpellier;;3,87952263361082;43,6071285339217",
+                "2;Hotel de Ville;1 place Georges Freche 34267 Montpellier;;3,89652239197876;43,5987299452849",
+                "3;Zoo de Lunaret;50 avenue Agropolis 34090 Mtp;;3,87388031141133;43,6395872778854"
+            };
+
+            _finder = new DefibrillatorFinder(longiture, latitude, defibLines);
+
+            Check.That(_finder.Find().Id).IsEqualTo("1");
+        }
+    }
+}
---
+++ codingame.defibrillators/codingame.defibrillators/Solution.cs
@@ -0,0 +1,105 @@
+﻿namespace codingame.defibrillators
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            string userLongitude = Console.ReadLine();
+            string userLatitude = Console.ReadLine();
+            int N = int.Parse(Console.ReadLine());
+            var debrilillatorLines = new List<string>();
+            for (int i = 0; i < N; i++)
+            {
+                debrilillatorLines.Add(Console.ReadLine().Trim());
+            }
+
+            var debrilillatorFinder = new DefibrillatorFinder(userLongitude, userLatitude, debrilillatorLines);
+
+            Console.WriteLine(debrilillatorFinder.Find().Name);
+        }
+
+        public static double ParseDouble(string userLongitude)
+        {
+            return double.Parse(userLongitude, new CultureInfo("fr-FR"));
+        }
+    }
+
+    public class DefibrillatorFinder
+    {
+        private readonly double _userLongitude;
+        private readonly double _userLatitude;
+        private readonly IEnumerable<Defibrillator> _debrilillators;
+
+        public DefibrillatorFinder(
+            string userLongitude,
+            string userLatitude,
+            IEnumerable<string> debrilillatorLines)
+        {
+            _userLongitude = Solution.ParseDouble(userLongitude);
+            _userLatitude = Solution.ParseDouble(userLatitude);
+            _debrilillators = debrilillatorLines.Select(Defibrillator.Parse);
+        }
+
+        public Defibrillator Find()
+        {
+            return _debrilillators.OrderBy(d => d.GetDistance(_userLongitude, _userLatitude)).First();
+        }
+    }
+
+    public class Defibrillator
+    {
+        protected Defibrillator(string[] splits)
+        {
+            Id = splits[0];
+            Name = splits[1];
+            Address = splits[2];
+            PhoneNumber = splits[3];
+            Longitude = Solution.ParseDouble(splits[4]);
+            Latitude = Solution.ParseDouble(splits[5]);
+        }
+
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Address { get; private set; }
+
+        public string PhoneNumber { get; private set; }
+
+        public double Longitude { get; private set; }
+
+        public double Latitude { get; private set; }
+
+        public static Defibrillator Parse(string defibrillatorLine)
+        {
+            var splits = defibrillatorLine.Split(';').Select(s => s.Trim());
+            return new Defibrillator(splits.ToArray());
+        }
+
+        public double GetDistance(double userLongitude, double userLatitude)
+        {
+            //x = (longB - longA)*cos((latA + latB)/2)
+            //y = latB - latA
+            //d = sqrt(x^2 + y^2)*6371
+            // b -> user
+            var x = (userLongitude.ToRadians() - Longitude.ToRadians()) * Math.Cos((userLatitude + Latitude) / 2);
+            var y = userLatitude.ToRadians() - Latitude.ToRadians();
+            return Math.Sqrt(x * x + y * y) * 6371;
+        }
+    }
+
+    public static class NumericExtensions
+    {
+        public static double ToRadians(this double val)
+        {
+            return (Math.PI / 180) * val;
+        }
+    }
+}
